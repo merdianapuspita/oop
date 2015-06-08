636792508710eaac5ba0b925570ebe44c8cd82b2
merdianapuspita <merdianapuspita@gmail.com>
2015-06-08 16:04:17 +0700

    Create cmilightcensor.java

---
+++ cmilightcensor.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class CmiLightCenCensor () {
+private int ketelitian = 15;
+private int sensitifitas 3;
+
+public CmiLightCensor () {
+int ketelitian = 5<=20;
+int sensitifitas = 1<=5;
+return new CmiLightCensor (ketelitian*sensitifitas)
+}
+}
