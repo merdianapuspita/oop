c581ba84cbe34f59e189d323e2f54b5a88de2a51
merdianapuspita <merdianapuspita@gmail.com>
2015-06-08 16:03:10 +0700

    Create cmiengine.java

---
+++ cmiengine.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class cmiEngine {
+private dayaKerja = 15;
+private jumlahSilinder = 3;
+
+public CmiEngine () {
+int jumalahSilinder = 1<=5;
+int dayaKerja = 10<=20;
+return new CmiEngine (jumlahSilinder*dayaKerja);
+}
+
+}
